d4d9413d3ab00b37c6b7d70b7e43b457e28eafde
Julia Skip <skipjulija01@gmail.com>
2025-06-10 14:32:35 +0300

    Initial Commit

---
+++ FoodClassifier_Skip_Filozop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodClassifier_Skip_Filozop
+//
+//  Created by Скіп Юлія Ярославівна on 10.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodClassifier_Skip_Filozop/FoodClassifier_Skip_FilozopApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodClassifier_Skip_FilozopApp.swift
+//  FoodClassifier_Skip_Filozop
+//
+//  Created by Скіп Юлія Ярославівна on 10.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodClassifier_Skip_FilozopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
